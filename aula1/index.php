afead186d376b8aaf32f1f9a02d3f9941f69c06f
UIrich <81659201+UIrich@users.noreply.github.com>
2022-08-09 22:31:31 -0300

    primeiro commit

---
+++ aula1/index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+<title>"Minhas redes sociais"</title>       
+    </head>
+<body>
+    <center><marquee><h1>Minhas redes sociais</marquee></h1></center><br>
+    <a href="https://pt-br.facebook.com/AnittaOficial">
+    <img src= "facebook.png" width = "50" height= "50">
+</a>
+    <a href="https://twitter.com/Anitta?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
+    <img src= "instagram.png" width = "50" height= "50">
+</a>
+    <a href="https://www.youtube.com/channel/UCqjjyPUghDSSKFBABM_CXMw">
+    <img src= "youtube.png" width = "50" height= "50">
+</a>
+    <a href="https://br.linkedin.com/in/anitta-hd-44913a146">
+    <img src= "linkedin.png" width = "50" height= "50">
+</a>
+</body>
+</html>
